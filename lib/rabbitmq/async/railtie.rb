bd7f6558b0dc521b1d586dea0d102ddacaa7b395
Gaurav Kumar // UPDATE THIS <gakmarn@amazon.com // UPDATE THIS>
2017-11-04 19:41:04 +0530

    added rake

--- lib/rabbitmq/async/railtie.rb
+++ lib/rabbitmq/async/railtie.rb
@@ -24,7 +24,7 @@
              async_service.perform()
             end
           rescue Interrupt => _
-            ch.close
+            channel.close
             conn.close
           end
         end
